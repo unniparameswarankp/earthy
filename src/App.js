4327afcf985ca799aa467555c6b564280684ee56
jo <jyothishkpk@gmail.com>
2025-05-08 19:42:05 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';  // Import the header component
+import Home from './pages/Home';
+import Footer from './components/Footer';
+import './styles/App.css';  // Importing the stylesheet
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <Header />  {/* Add the Header component here */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* You can add more routes here, like /products, /cart, etc. */}
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
